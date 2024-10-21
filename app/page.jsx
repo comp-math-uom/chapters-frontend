ebc3ca6af63985fdba316af17c160494ad981bda
Hirusha Fernando <heshanhfernando@gmail.com>
2024-10-21 09:19:51 +0530

    UI changes in portfolio

--- app/page.jsx
+++ app/page.jsx
@@ -6,10 +6,6 @@ export default function Home() {
     return (
         <div className="container-fluid flex flex-col justify-center m-auto">
             <Navbar />
-            
-            <div className="container flex flex-col justify-center m-auto">
-                <Header />                
-            </div>
         </div>
     );
 }
