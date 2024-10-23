587d88812397e1bade95b03735f711b724b0070b
Hirusha Fernando <heshanhfernando@gmail.com>
2024-10-23 16:20:55 +0530

    Code Refactoring And Added Loading Spinner

--- app/components/common/Navbar.jsx
+++ app/components/common/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
       <div>
         <div className="flex items-center justify-between p-5 container m-auto">
           <div>
-            <Link href="/" passHref>
+            <Link href="/public" passHref>
               <h1 className="m-0 text-3xl font-impact cursor-pointer">CHAPTERS</h1>
             </Link>
           </div>
