bf6cee38007b931dadd3d15bca3bc866843a66bf
Hirusha Fernando <heshanhfernando@gmail.com>
2024-11-13 21:35:03 +0530

    blog home page

---
+++ app/data/blogPreviews.js
@@ -0,0 +1,74 @@
+export const blogPreviews = [
+    {
+        id: 1,
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum is simply dummy text of the printing and typesetting industry.",
+        link: "/blog/1",
+        coverImage: "https://picsum.photos/seed/picsum/400/300",
+        avatar: "https://avatar.iran.liara.run/public",
+        author: "Segun Adebayo"
+    },
+    {
+        id: 2,
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum is simply dummy text of the printing and typesetting industry.",
+        link: "/blog/2",
+        coverImage: "https://picsum.photos/seed/picsum/400/300",
+        avatar: "https://avatar.iran.liara.run/public",
+        author: "John Doe"
+    },
+    {
+        id: 3,
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum is simply dummy text of the printing and typesetting industry.",
+        link: "/blog/3",
+        coverImage: "https://picsum.photos/seed/picsum/400/300",
+        avatar: "https://avatar.iran.liara.run/public",
+        author: "Jane Doe"
+    },
+    {
+        id: 4,
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum is simply dummy text of the printing and typesetting industry.",
+        link: "/blog/4",
+        coverImage: "https://picsum.photos/seed/picsum/400/300",
+        avatar: "https://avatar.iran.liara.run/public",
+        author: "Jack Doe"
+    },
+    {
+        id: 5,
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum is simply dummy text of the printing and typesetting industry.",
+        link: "/blog/5",
+        coverImage: "https://picsum.photos/seed/picsum/400/300",
+        avatar: "https://avatar.iran.liara.run/public",
+        author: "Jill Doe"
+    },
+    {
+        id: 6,
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum is simply dummy text of the printing and typesetting industry.",
+        link: "/blog/6",
+        coverImage: "https://picsum.photos/seed/picsum/400/300",
+        avatar: "https://avatar.iran.liara.run/public",
+        author: "J.K. Rowling"
+    },
+    {
+        id: 7,
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum is simply dummy text of the printing and typesetting industry.",
+        link: "/blog/7",
+        coverImage: "https://picsum.photos/seed/picsum/400/300",
+        avatar: "https://avatar.iran.liara.run/public",
+        author: "J.R.R. Tolkien"
+    },
+    {
+        id: 8,
+        title: "Lorem Ipsum",
+        description: "Lorem ipsum is simply dummy text of the printing and typesetting industry.",
+        link: "/",
+        coverImage: "https://picsum.photos/seed/picsum/400/300",
+        avatar: "https://avatar.iran.liara.run/public",
+        author: "R.R. Martin"
+    },
+]
