e87c9873cc95eadd588f524efb29e4799b91c882
Hirusha Fernando <heshanhfernando@gmail.com>
2024-11-15 22:38:02 +0530

    404 page change

--- app/(blog)/blog/[id]/page.jsx
+++ app/(blog)/blog/[id]/page.jsx
@@ -1,7 +1,14 @@
-export default function Page() {
-    return (
-        <div className="container flex flex-wrap justify-center m-auto px-20 gap-20">
+import { notFound } from 'next/navigation';
 
+export default function Page({ params }) {
+
+    if (params.id !== "69") {
+        return notFound();
+    }
+
+    return (
+        <div className="container flex flex-wrap justify-center m-auto px-20 gap-20 mt-20">
+            {params.id}
         </div>
     );
 }
