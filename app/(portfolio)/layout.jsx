012ce123cc57694e40839425b7a9f45a334c66e3
Hirusha Fernando <heshanhfernando@gmail.com>
2024-11-15 22:49:00 +0530

    Loading page

--- app/(portfolio)/layout.jsx
+++ app/(portfolio)/layout.jsx
@@ -1,7 +1,7 @@
 import React, {Suspense} from "react";
 import Navbar from "@/app/components/common/Navbar";
 import Footer from "@/app/components/common/Footer";
-import Loading from "@/app/(portfolio)/loading";
+import Loading from "@/app/loading";
 
 export const metadata = {
     title: "CHAPTERS | Portfolio",
--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,22 @@
+import {Flex, Spinner, Text} from "@chakra-ui/react";
+
 export default function Loading() {
     return (
-        <div>
-            Loading...
-        </div>
+        <Flex
+            h="calc(100vh - 484px)"
+            alignItems="center"
+            justifyContent="center"
+            direction="column"
+        >
+            <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="black"
+                size="xl"
+                mb={5}
+            />
+            <Text>Please wait...</Text>
+        </Flex>
     );
 }
