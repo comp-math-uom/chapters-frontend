15fcee8578f009494006052a6441d5524167d18b
Lihaj <lihajwickramasinghe@gmail.com>
2024-11-18 19:17:59 +0530

    Hashtags are added to the BlogHeader.

--- app/components/blog/BlogHeader.jsx
+++ app/components/blog/BlogHeader.jsx
@@ -1,22 +1,31 @@
-import { Box, Heading, HStack, VStack, Text } from '@chakra-ui/react';
+import { Box, Heading, HStack, VStack, Text,Flex,Tag } from '@chakra-ui/react';
 import { BiLike, BiComment } from 'react-icons/bi';
 import { BsShare } from 'react-icons/bs';
 
-export default function BlogHeader({title,author,date,info,likes,comments}){
+export default function BlogHeader({title,author,date,likes,comments,hashtags}){
     return(
         <Box width={'full'} py={16}>
             <VStack spacing={2} align={"left"}>
                 <Heading as="h1" size="4xl" fontWeight="bold" className={'font-anton'}>
                     {title}
                 </Heading>
-                <HStack spacing={2} color="gray.600">
+                <HStack spacing={2} color="gray.600" fontSize={"xl"}>
                     <Text>Written by {author}</Text>
                     <Text>|</Text>
                     <Text>{date}</Text>
-                    <Text>|</Text>
-                    <Text>{info}</Text>
                 </HStack>
-                <HStack marginTop={8} spacing={6}>
+                <Flex wrap="wrap" gap={1} marginTop={-2}>
+                    {hashtags.map((tag, index) => (
+                        <Text
+                            key={index}
+                            fontSize="m"
+                            color="gray.500"
+                        >
+                            #{tag}
+                        </Text>
+                    ))}
+                </Flex>
+                <HStack marginTop={6} spacing={6}>
                     <HStack className="cursor-pointer hover:opacity-80">
                         <BiLike size={23} className="text-gray-600" />
                         <Text fontSize="sm" color="gray.600">{likes}</Text>
