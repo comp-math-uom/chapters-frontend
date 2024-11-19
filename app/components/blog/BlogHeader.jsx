5f83f39e0635c03ca8c1ea5cac8e8e4fd342114c
Lihaj <lihajwickramasinghe@gmail.com>
2024-11-20 03:45:34 +0530

    Blog Header is complete!

--- app/components/blog/BlogHeader.jsx
+++ app/components/blog/BlogHeader.jsx
@@ -1,38 +1,43 @@
-import { Box, Heading, HStack, VStack, Text,Flex,Tag } from '@chakra-ui/react';
+import {Box, Heading, HStack, VStack, Text, Flex, Tag, Avatar} from '@chakra-ui/react';
 import { BiLike, BiComment } from 'react-icons/bi';
 import { BsShare } from 'react-icons/bs';
 
-export default function BlogHeader({title,author,date,likes,comments,hashtags}){
+export default function BlogHeader({blog}){
     return(
         <Box width={'full'} py={16}>
             <VStack spacing={2} align={"left"}>
                 <Heading as="h1" size="4xl" fontWeight="bold" className={'font-anton'}>
-                    {title}
+                    {blog.title}
                 </Heading>
-                <HStack spacing={2} color="gray.600" fontSize={"xl"}>
-                    <Text>Written by {author}</Text>
-                    <Text>|</Text>
-                    <Text>{date}</Text>
+                <HStack spacing={2}>
+                    <Avatar src={blog.avatar}/>
+                    <VStack align={"left"}>
+                        <HStack spacing={2} color="gray.600" fontSize={"xl"}>
+                            <Text>Written by {blog.author}</Text>
+                            <Text>|</Text>
+                            <Text>{blog.date}</Text>
+                        </HStack>
+                        <Flex wrap="wrap" gap={2} marginTop={-3}>
+                            {blog.hashtags.map((tag, index) => (
+                                <Text
+                                    key={index}
+                                    fontSize="m"
+                                    color="gray.500"
+                                >
+                                    #{tag}
+                                </Text>
+                            ))}
+                        </Flex>
+                    </VStack>
                 </HStack>
-                <Flex wrap="wrap" gap={1} marginTop={-2}>
-                    {hashtags.map((tag, index) => (
-                        <Text
-                            key={index}
-                            fontSize="m"
-                            color="gray.500"
-                        >
-                            #{tag}
-                        </Text>
-                    ))}
-                </Flex>
                 <HStack marginTop={6} spacing={6}>
                     <HStack className="cursor-pointer hover:opacity-80">
                         <BiLike size={23} className="text-gray-600" />
-                        <Text fontSize="sm" color="gray.600">{likes}</Text>
+                        <Text fontSize="sm" color="gray.600">{blog.likes}</Text>
                     </HStack>
                     <HStack spacing={1} className="cursor-pointer hover:opacity-80">
                         <BiComment size={23} className="text-gray-600" />
-                        <Text fontSize="sm" color="gray.600">{comments}</Text>
+                        <Text fontSize="sm" color="gray.600">{blog.comments}</Text>
                     </HStack>
                     <HStack spacing={1} className="cursor-pointer hover:opacity-80">
                         <BsShare size={20} className="text-gray-600" />
---
+++ app/data/blog.js
@@ -0,0 +1,34 @@
+export const blog = [
+    {
+        id: 1,
+        title: "Evolution of AI",
+        author: "S.S.Kulathunga",
+        date: "June 06 2024",
+        likes: 23,
+        comments: 3,
+        avatar: "https://avatar.iran.liara.run/public",
+        hashtags: ['history', 'AI', 'evolution']
+    },
+    {
+        id: 2,
+        title: "AI Winter",
+        author: "L.K Samarasinghe",
+        date: "June 07 2024",
+        likes: 45,
+        comments: 4,
+        avatar: "https://avatar.iran.liara.run/public",
+        hashtags: ['history','evolution']
+    },
+    {
+        id: 3,
+        title: "Ed Sheeran",
+        author: "J.K Kumara",
+        date: "June 07 2024",
+        likes: 45,
+        comments: 4,
+        avatar: "https://avatar.iran.liara.run/public",
+        hashtags: ['Music', 'Guitar', 'evolution']
+    },
+]
+
+export default blog;
