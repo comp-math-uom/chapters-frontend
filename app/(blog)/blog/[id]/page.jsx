649c31c58bea877f8978f625d9ee497ae363ac78
Lihaj <lihajwickramasinghe@gmail.com>
2024-11-20 03:45:34 +0530

    Blog Header is complete!

--- app/(blog)/blog/[id]/page.jsx
+++ app/(blog)/blog/[id]/page.jsx
@@ -1,21 +1,17 @@
 import { notFound } from 'next/navigation';
+import BlogHeader from "@/app/components/blog/BlogHeader";
+import blogService from "@/app/services/blogService";
 
-export default function Page({ params }) {
 
-    if (params.id !== "69") {
+export default async function Page({ params }) {
+    const blogId = Number(params.id);
+    const blog = await blogService.getBlog(blogId);
+    if (!blog) {
         return notFound();
     }
-
     return (
         <div className="container flex flex-wrap justify-center m-auto px-20 gap-20">
-            <BlogHeader
-                title="Evolution of AI"
-                author="S.S.Kulathunga"
-                date="June 06 2024"
-                likes={23}
-                comments={3}
-                hashtags={['history','AI','evolution']}
-            />
+            <BlogHeader blog={blog}/>
         </div>
     );
 }
