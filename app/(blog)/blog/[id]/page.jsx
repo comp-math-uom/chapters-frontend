63b67ef5bc1cdb9e7f74e0799e167985db52a59a
Lihaj <lihajwickramasinghe@gmail.com>
2025-03-23 00:32:45 +0530

    Blog Content is added.

--- app/(blog)/blog/[id]/page.jsx
+++ app/(blog)/blog/[id]/page.jsx
@@ -1,6 +1,7 @@
 import { notFound } from 'next/navigation';
 import BlogHeader from "@/app/components/blog/BlogHeader";
 import blogService from "@/app/services/blogService";
+import BlogContent from "@/app/components/blog/BlogContent";
 
 
 export default async function Page({ params }) {
@@ -12,6 +13,7 @@ export default async function Page({ params }) {
     return (
         <div className="container flex flex-wrap justify-center m-auto px-20 gap-20">
             <BlogHeader blog={blog}/>
+            <BlogContent blog={blog}/>
         </div>
     );
 }
