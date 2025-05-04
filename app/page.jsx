01475de9ab33be9497f5b068f9b9316489bf445c
Hirusha Fernando <heshanhfernando@gmail.com>
2025-05-04 19:46:16 +0530

    Add Forum and Portfolio components to enhance landing page structure

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,8 @@ import HeroSection from "./components/landing/HeroSection";
 import ContactUs from "@/app/components/landing/ContactUs";
 import WeAre from "@/app/components/landing/WeAre";
 import Blog from "@/app/components/landing/Blog";
-// import AchievementsSection from "./components/landing/AchievementsSection";
+import Forum from "@/app/components/landing/Forum";
+import Portfolio from "@/app/components/landing/Portfolio";
 
 export default function Home() {
     return (
@@ -12,10 +13,11 @@ export default function Home() {
             <Navbar/>
             <main className="flex flex-col w-full container px-20 mt-20">
                 <HeroSection />
+                <Portfolio />
+                <Forum />
                 <Blog />
                 <WeAre />
                 <ContactUs />
-                {/* You can add more sections here as you develop them */}
             </main>
         </div>
     );
