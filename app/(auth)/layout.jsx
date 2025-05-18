d6b8d9d5f7860a945701def590cfdd4ac2ebfaad
Hirusha Fernando <heshanhfernando@gmail.com>
2025-05-18 13:14:58 +0530

    Auth responsive

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,7 +1,9 @@
 export default function AuthLayout({children}) {
     return (
-        <div className="flex flex-col min-h-screen">
-            {children}
+        <div className="flex flex-col min-h-screen w-full overflow-x-hidden bg-gray-50">
+            <main className="flex-grow flex items-center justify-center py-8 px-4 sm:px-6 lg:px-8">
+                {children}
+            </main>
         </div>
     );
 }
