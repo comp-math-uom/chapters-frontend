4eb5a3ef33179b13acc9a9354387220831469b1e
PERERA WJR <janithravisankax@gmail.com>
2025-07-13 10:07:38 +0530

    keycloak integrations

--- app/providers/Providers.jsx
+++ app/providers/Providers.jsx
@@ -2,13 +2,70 @@
 
 import { NavigationProvider } from "@/app/providers/NavigationProvider";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect, useState, createContext, useContext } from 'react';
+import { getKeycloakInstance } from "@/app/lib/services/keycloak";
+
+const KeycloakContext = createContext();
+
+export function useKeycloak() {
+    return useContext(KeycloakContext);
+}
+
+function KeycloakProvider({ children }) {
+    const [keycloak, setKeycloak] = useState(null);
+    const [initialized, setInitialized] = useState(false);
+
+    useEffect(() => {
+        const kc = getKeycloakInstance();
+        if (!kc.__initialized) {
+            kc.init({ onLoad: 'check-sso', pkceMethod: 'S256', silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html' })
+                .then(authenticated => {
+                    setInitialized(true);
+                    if (authenticated) {
+                        localStorage.setItem('kc_access_token', kc.token);
+                        localStorage.setItem('kc_refresh_token', kc.refreshToken);
+                    }
+                })
+                .catch(() => setInitialized(true));
+            kc.__initialized = true;
+        } else {
+            setInitialized(true);
+        }
+        setKeycloak(kc);
+    }, []);
+
+    useEffect(() => {
+        if (keycloak) {
+            keycloak.onAuthSuccess = () => {
+                localStorage.setItem('kc_access_token', keycloak.token);
+                localStorage.setItem('kc_refresh_token', keycloak.refreshToken);
+            };
+            keycloak.onTokenRefresh = () => {
+                localStorage.setItem('kc_access_token', keycloak.token);
+                localStorage.setItem('kc_refresh_token', keycloak.refreshToken);
+            };
+            keycloak.onAuthLogout = () => {
+                localStorage.removeItem('kc_access_token');
+                localStorage.removeItem('kc_refresh_token');
+            };
+        }
+    }, [keycloak]);
+
+    return (
+        <KeycloakContext.Provider value={{ keycloak, initialized }}>
+            {children}
+        </KeycloakContext.Provider>
+    );
+}
 
 export function Providers({children}) {
     return (
-        <NavigationProvider>
-            <ChakraProvider>
-                {children}
-            </ChakraProvider>
-        </NavigationProvider>
+        <KeycloakProvider>
+            <NavigationProvider>
+                <ChakraProvider>
+                    {children}
+                </ChakraProvider>
+            </NavigationProvider>
+        </KeycloakProvider>
     );
 }
