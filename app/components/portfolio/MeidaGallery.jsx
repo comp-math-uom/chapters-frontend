ef46f48a84a2d1a649af7f24957bf06ac4340f1f
Lihaj <lihajwickramasinghe@gmail.com>
2025-08-22 13:29:57 +0530

    axios default headers done.

--- app/components/portfolio/MeidaGallery.jsx
+++ app/components/portfolio/MeidaGallery.jsx
@@ -11,9 +11,6 @@ export default function MediaGallery({galleryItems}) {
             layout="rows"
             renderPhoto={GalleryItem}
             defaultContainerWidth={1200}
-            sizes={{
-                size: "(max-width: 640px) calc(100vw - 16px), (max-width: 1024px) calc(100vw - 48px), calc(100vw - 240px)"
-            }}
             spacing={10}
             padding={0}
         />
